eed0a30d26cda2e4b7e1f818f507a9e4008c4711
Lachezar Stamenov <lachezar.stamenov@abv.bg>
2022-07-04 11:31:28 +0300

    single inheritance task lodade

--- 05_Inheritance/01_food/project/fruit.py
+++ 05_Inheritance/01_food/project/fruit.py
@@ -3,7 +3,7 @@
 # •	In the fruit.py file, create a class called Fruit with will receive a name (str) and an expiration_date (str) upon
 # initialization.
 # Fruit should inherit from Food.
-from project.food import Food
+from .food import Food
 
 
 class Fruit(Food):
---
+++ 05_Inheritance/02_single_inheritance/project/animal.py
@@ -0,0 +1,12 @@
+# In a folder called project create two files: animal.py and dog.py:
+# •	In the animal.py file, create a class called Animal with a single method eat() that returns: "eating…".
+# •	In the dog.py file, create a class called Dog with a single method bark() that returns: "barking…".
+# The Dog should inherit from Animal.
+
+class Animal:
+
+    def __init__(self):
+        pass
+
+    def eat(self):
+        return "eating..."
