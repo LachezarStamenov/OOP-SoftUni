bc73aa5164b5c101436679f221c2d383cfef9e38
Lachezar Stamenov <lachezar.stamenov@abv.bg>
2022-07-05 19:19:20 +0300

    need for speed task loaded

---
+++ 06_Inheritance_exercises/04_Need_fror_speed/project/family_car.py
@@ -0,0 +1,9 @@
+from .car import Car
+
+
+class FamilyCar(Car):
+
+    def __init__(self, fuel, horse_power):
+        super().__init__(fuel, horse_power)
+
+
---
+++ 06_Inheritance_exercises/04_Need_fror_speed/project/vehicle.py
@@ -0,0 +1,28 @@
+# Create a base class Vehicle. It should contain the following attributes:
+# •	DEFAULT_FUEL_CONSUMPTION: float (constant)
+# •	fuel_consumption: float - represents the fuel consumption per kilometer
+# •	fuel: float - represents the quantity of fuel in a specific vehicle
+# •	horse_power: int
+# Upon initialization, the class should receive fuel and horse_power. The DEFAULT_FUEL_CONSUMPTION value should be
+# set to the fuel_consumption value.
+# Each class should have the following methods:
+# •	drive(kilometers) - reduces the fuel based on the traveled kilometers and fuel consumption (km * fuel consumption).
+# Keep in mind that you can start driving the vehicle only if you have enough fuel to finish the driving.
+# The default fuel consumption for the different vehicles is:
+# •	Vehicle is 1.25
+# •	SportCar is 10
+# •	RaceMotorcycle is 8
+# •	Car is 3
+
+class Vehicle:
+    DEFAULT_FUEL_CONSUMPTION = 1.25
+
+    def __init__(self, fuel, horse_power):
+        self.fuel = fuel
+        self.horse_power = horse_power
+        self.fuel_consumption = self.DEFAULT_FUEL_CONSUMPTION
+
+    def drive(self, kilometers):
+        needed_fuel = self.fuel_consumption * kilometers
+        if self.fuel >= needed_fuel:
+            self.fuel -= needed_fuel
